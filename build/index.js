b9fa4ffe8c23ba14c4103d682f0eba199646cec7
Shohei Nakagawa <hukusuke1007@gmail.com>
2021-10-12 00:50:55 +0900

    update

--- build/index.js
+++ build/index.js
@@ -1,2 +1,12 @@
-console.log('test');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const yaml = require("js-yaml");
+const yamlDir = `${process.cwd()}/sample_yaml`;
+const execute = async () => {
+    const bytes = fs.readFileSync(`${yamlDir}/sample.yaml`, 'utf8');
+    const data = yaml.load(bytes);
+    console.log(data);
+};
+execute();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('test')
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
+
+const yamlDir = `${process.cwd()}/sample_yaml`
+
+const execute = async () => {
+  const bytes = fs.readFileSync(`${yamlDir}/sample.yaml`, 'utf8')
+  const data = yaml.load(bytes)
+  console.log(data)
+}
+
+execute()
